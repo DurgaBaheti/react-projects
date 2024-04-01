74adbebe7e1c460a398d1c84782ddef36ae52f98
dheerajbaheti <145109215+DurgaBaheti@users.noreply.github.com>
2024-04-01 17:53:47 +0000

    currencyconverter - custom hook, inputBox done

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react-projects
 projects, while learning react
+
+
+# for removing errors of non-used variables
+    /* eslint-disable react/prop-types */
+    /* eslint-disable no-unused-vars */ 
---
+++ currency-converter/src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+
+import { useState } from "react";
+import "./App.css";
+import { InputBox } from "./components/InputBox";
+
+function App() {
+  return (
+    <div className="h-screen width-full text-center flex justify-center align-center bg-black text-white">
+      <h1 className="text-3xl">
+        Dheeraj Baheti
+      </h1>
+    </div>
+  );
+}
+
+export default App;
