57d458f42f80421aa3104e7f74bb0c0cad443664
dheerajbaheti <145109215+DurgaBaheti@users.noreply.github.com>
2024-04-09 10:44:55 +0000

    store configured

--- mega-project/src/appwrite/auth.js
+++ mega-project/src/appwrite/auth.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-useless-catch */
-import conf from "../conf.js";
+import conf from "../conf/conf.js";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
@@ -7,55 +6,48 @@ export class AuthService {
   account;
 
   constructor() {
-    this.client.setEndpoint(conf.endpoint).setProject(conf.appwriteProjectId);
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
     this.account = new Account(this.client);
   }
 
   async createAccount({ email, password, name }) {
-    // eslint-disable-next-line no-useless-catch
-    try {
-      const userAccount = await this.account.create(
-        ID.unique(),
-        email,
-        password,
-        name
-      );
-      // eslint-disable-next-line no-empty
-      if (userAccount) {
-        return this.Login({ email, password });
-      } else {
-        return userAccount;
-      }
-    } catch (error) {
-      throw error;
+    const userAccount = await this.account.create(
+      ID.unique(),
+      email,
+      password,
+      name
+    );
+    if (userAccount) {
+      // call another method
+      return this.login({ email, password });
+    } else {
+      return userAccount;
     }
   }
 
-  async Login({ email, password }) {
-    try {
-      return await this.account.createEmailPasswordSession(email, password);
-    } catch (error) {
-      throw error;
-    }
+  async login({ email, password }) {
+    return await this.account.createEmailSession(email, password);
   }
 
   async getCurrentUser() {
     try {
-      return await this.account.getSession();
+      return await this.account.get();
     } catch (error) {
-      console.log("Appwrite server :: getCurrentUser :: error", error);
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
     }
+
     return null;
   }
 
   async logout() {
     try {
-        await this.account.deleteSessions();
+      await this.account.deleteSessions();
     } catch (error) {
-        console.log("Appwrite server :: getCurrentUser :: error", error);
+      console.log("Appwrite serive :: logout :: error", error);
     }
   }
-
 }
 
 const authService = new AuthService();
---
+++ mega-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function Footer() {
+    return (
+            <h1>footer</h1>
+    )
+}
+
+
+export default Footer
