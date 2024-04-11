5d8de855d797712c6da302e14523b1157d7c3f25
dheerajbaheti <145109215+DurgaBaheti@users.noreply.github.com>
2024-04-11 10:52:10 +0000

    RTE, PostForm component done

--- mega-project/src/components/index.js
+++ mega-project/src/components/index.js
@@ -1,12 +1,14 @@
-import Container from "../container/Container";
+import Container from "../Container/Container";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import LogoutBtn  from "./Header/LogoutBtn";
+import postForm from "./PostForm/PostForm"
 import Button from "./Button";
 import Input from "./Input";
 import Login from "./Login";
 import Logo  from "./Logo";
 import PostCard from "./PostCard"
+import RTE from "./RTE";
 import Select from "./Select";
 
 
@@ -15,10 +17,12 @@ export {
     Footer,
     Header,
     LogoutBtn,
+    postForm,
     Button,
     Input,
     Login,  
     Logo,
     PostCard,
+    RTE,
     Select,
 }
